cef9db0330672b4910da771c361bb6530bae138f
Yano Hinako <nikaidohina0909@gmail.com>
2024-10-09 18:20:34 +0900

    Merge remote-tracking branch 'origin/main'

--- 20241009/test.js
+++ 20241009/test.js
@@ -1,2 +1,15 @@
 console.log("原神のアップデートがもうすぐです。")
 console.log("ガチャ更新が楽しみだね！！")
+
+let variableA;
+variableA="最初の代入A"
+console.log(variableA);
+
+let variableB="最初の代入B";
+console.log(variableB);
+
+variableA="二回目の代入A";
+console.log(variableA);
+
+variableA=variableB;
+console.log(variableA);
