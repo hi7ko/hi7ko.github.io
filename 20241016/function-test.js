e66967bce246526d900a12ab7d66c999d6b05a5c
Yano Hinako <nikaidohina0909@gmail.com>
2024-10-16 17:54:34 +0900

    Merge remote-tracking branch 'origin/main'

--- 20241016/function-test.js
+++ 20241016/function-test.js
@@ -1,3 +1,10 @@
 function logMessage() {
     console.log("LogMessageが呼び出されました。");
+}function logMessage2(message){
+    console.log(`logMessage2: ${message}`);
 }
+// テンプレート変数を使わない場合
+console.log("logMessage2: " + message);
+
+// テンプレート変数を使った場合
+console.log(`logMessage2: ${message}`);
