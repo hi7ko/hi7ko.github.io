a3819b973bd31c82254128dbef3d87cde9ed8aa5
Yano Hinako <nikaidohina0909@gmail.com>
2024-11-06 17:56:22 +0900

    Merge remote-tracking branch 'origin/main'

---
+++ 20241106/fizzbuzz.js
@@ -0,0 +1,37 @@
+function doFizzBuzz(){
+    let answer = document.querySelector("#answer");
+
+    // 解1：単純にこれまで通り実装する
+    // let i = 1;
+    // while (i <= 100) {
+    //   if((i % 3 == 0) && (i % 5 == 0)){
+    //     answer.innerHTML += `FizzBuzz<br>`;
+    //   }else if(i % 3 == 0){
+    //     answer.innerHTML += `Fizz<br>`;
+    //   }else if(i % 5 == 0){
+    //     answer.innerHTML += `Buzz<br />`;
+    //   }else {
+    //     answer.innerHTML += `${i}<br>`;
+    //   }
+    //   i++;
+    // }
+
+    // 解2：FizzBuzz判定を関数化して処理を分ける
+    let i = 1;
+    while (i <= 100) {
+        answer.innerHTML += checkFizzBuzz(i);
+        i++;
+    }
+}
+
+function checkFizzBuzz(i){
+    if((i % 3 == 0) && (i % 5 == 0)){
+        return `FizzBuzz<br>`;
+    }else if(i % 3 == 0){
+        return `Fizz<br>`;
+    }else if(i % 5 == 0){
+        return `Buzz<br />`;
+    }else {
+        return `${i}<br>`;
+    }
+}
