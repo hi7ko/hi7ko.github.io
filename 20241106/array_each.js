1a80380222fb45df986096ef52718e332f195af0
Yano Hinako <nikaidohina0909@gmail.com>
2024-11-06 18:32:08 +0900

    Merge remote-tracking branch 'origin/main'

--- 20241106/array_each.js
+++ 20241106/array_each.js
@@ -1,11 +1,33 @@
-let answer=document.querySelector('#answer');
+let answer = document.querySelector('#answer');
 
-//while
-let i=0;
-let html='';
-while (i<fruitNames.length) {
-    html+='<li>${fruitNames[i]}</li>';
-    i++;
-}
-answer.innerHTML='<ul>${html}</ul>';
+// 最終的に作りたいHTML
+// <ul>
+//   <li>りんご</li>
+//   <li>みかん</li>
+//   <li>いちご</li>
+//   <li>いちじく</li>
+// </ul>
 
+// // while
+ let i = 0;
+ let html = '';
+while(i < fruitNames.length) {
+   html += `<li>${fruitNames[i]}</li>`;
+   i++;
+ }
+ answer.innerHTML = `<ul>${html}</ul>`;
+
+// // for
+// let i = 0;
+// let html = '';
+// for(let i = 0;i < fruitNames.length;i++){
+//   html += `<li>${fruitNames[i]}</li>`;
+// }
+// answer.innerHTML = `<ul>${html}</ul>`;
+
+// Array.forEach
+let html = '';
+fruitNames.forEach(function(fruitName){
+    html += `<li>${fruitName}</li>`;
+});
+answer.innerHTML = `<ul>${html}</ul>`;
