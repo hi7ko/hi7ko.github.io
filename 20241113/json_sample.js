a19d118a1be39c697e39c88922269184a16bcd54
Yano Hinako <nikaidohina0909@gmail.com>
2024-11-13 18:08:25 +0900

    Merge remote-tracking branch 'origin/main'

--- 20241113/json_sample.js
+++ 20241113/json_sample.js
@@ -7,8 +7,6 @@ fetch("https://morimorihoge.github.io/teu2024f/tweets.json").then(
         json.forEach(
             (tweet) => {
                 document.querySelector("#answer").innerHTML += `${tweet.name}<br>`;
-
-                document.querySelector("#answer").innerHTML = json[0].name;
             }
         )
     }
