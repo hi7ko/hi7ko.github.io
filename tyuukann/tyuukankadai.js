18e7912d481c7b4761bf853005b97d5cdd7375dc
Yano Hinako <nikaidohina0909@gmail.com>
2024-11-19 18:05:46 +0900

    Merge remote-tracking branch 'origin/main'

--- tyuukann/tyuukankadai.js
+++ tyuukann/tyuukankadai.js
@@ -3,8 +3,8 @@ answer.innerHTML ="";
 let html = "";
 while(i < tweets.length){
     html += `<div class="clearfix border border-secondary me-2 p-2">
-        <div class="float-start border me-2"><img src="${tweets[i].avatar}" class="tweets-photo" alt="png"></div>
-        <b>${tweets[i].name}</b> ${tweets[i].message} <div class="tweets-At">${tweets[i].tweetedAt}</div>
+        <div class="float-start border me-2"><b>${tweets[i].name}</b> ${tweets[i].message} </div>
+        <div class="tweets-At">${tweets[i].tweetedAt}</div><img src="${tweets[i].avatar}" class="tweets-photo" alt="png">
     </div>`
     i++;
 }
@@ -16,10 +16,9 @@ function All() {
     html = "";
     while(i < tweets.length){
         html += `<div class="border border-secondary me-2 p-2">
-        <div class="border me-2"><img src="${tweets[i].avatar}" class="tweets-photo" alt="png">
-        <b>${tweets[i].name}</b> ${tweets[i].message} <div class="tweets-At">${tweets[i].tweetedAt}</div>
-    </div>
-</div>`
+        <div class="float-start border me-2"><b>${tweets[i].name}</b> ${tweets[i].message} </div>
+        <div class="tweets-At">${tweets[i].tweetedAt}</div><img src="${tweets[i].avatar}" class="tweets-photo" alt="png">
+    </div>`
         i++;
     }
     answer.innerHTML = `${html}`;
@@ -34,8 +33,8 @@ function saburou(){
     while (i < tweets.length) {
         if (tweets[i].name === "三郎BOT") {
             html += `<div class="clearfix border border-secondary me-2 p-2">
-        <div class="float-start border me-2"><img src="${tweets[i].avatar}" class="tweets-photo" alt="png"></div>
-        <b>${tweets[i].name}</b> ${tweets[i].message} <div class="tweets-At">${tweets[i].tweetedAt}</div>
+        <div class="float-start border me-2"><b>${tweets[i].name}</b> ${tweets[i].message} </div>
+        <div class="tweets-At">${tweets[i].tweetedAt}</div><img src="${tweets[i].avatar}" class="tweets-photo" alt="png">
     </div>`
         }
         i++;
@@ -51,8 +50,8 @@ function tarou(){
     while (i < tweets.length) {
         if (tweets[i].name === "太郎") {
             html += `<div class="clearfix border border-secondary me-2 p-2">
-        <div class="float-start border me-2"><img src="${tweets[i].avatar}" class="tweets-photo" alt="png"></div>
-        <b>${tweets[i].name}</b> ${tweets[i].message} <div class="tweets-At">${tweets[i].tweetedAt}</div>
+        <div class="float-start border me-2"><b>${tweets[i].name}</b> ${tweets[i].message} </div>
+        <div class="tweets-At">${tweets[i].tweetedAt}</div><img src="${tweets[i].avatar}" class="tweets-photo" alt="png">
     </div>`
         }
         i++;
@@ -68,8 +67,8 @@ function jirou(){
     while (i < tweets.length) {
         if (tweets[i].name === "次郎") {
             html += `<div class="clearfix border border-secondary me-2 p-2">
-        <div class="float-start border me-2"><img src="${tweets[i].avatar}" class="tweets-photo" alt="png"></div>
-        <b>${tweets[i].name}</b> ${tweets[i].message} <div class="tweets-At">${tweets[i].tweetedAt}</div>
+        <div class="float-start border me-2"><b>${tweets[i].name}</b> ${tweets[i].message} </div>
+        <div class="tweets-At">${tweets[i].tweetedAt}</div><img src="${tweets[i].avatar}" class="tweets-photo" alt="png">
     </div>`
         }
         i++;
